203d44e138a7215b6c02a66051c60c19b39e3cf0
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-10-06 20:59:45 +0200

    Start on a script testing all the methods

---
+++ examples/testall.js
@@ -0,0 +1,81 @@
+/* LysKOM protocol A: test all requests and messages
+ *
+ * Copyright 2014 Peter Liljenberg <peter.liljenberg@gmail.com>
+ *
+ * Distributed under an MIT license, please see LICENSE in the top dir.
+ */
+
+'use strict';
+
+//
+// This runs ONLY against localhost, since you shouldn't run this
+// script against any shared LysKOM server (unless you really want to
+// annoy people.)
+//
+
+var Promise = require('bluebird');
+
+var lyskom = require('../index.js');
+
+var getAsyncHandler = function(name) {
+    return function(msg) {
+        console.log('%s: %j', name, msg);
+    };
+};
+
+var client;
+
+console.log('Connecting to 127.0.0.1');
+lyskom.connectAndWait({host: '127.0.0.1'})
+    .then(function(c) {
+        client = c;
+
+        client.on('error', function(err) {
+            console.error('Connection error: %s', err);
+            process.exit(1);
+        });
+
+        client.on('close', function() {
+            console.error('Connection closed');
+            process.exit(0);
+        });
+
+        // Listen to all async messages, all the time
+        var acceptAsync = [];
+        for (var name in lyskom.async) {
+            if (lyskom.async.hasOwnProperty(name)) {
+                var num = lyskom.async[name];
+                if (typeof num === 'number') {
+                    acceptAsync.push(num);
+                    client.on(name, getAsyncHandler(name));
+                }
+            }
+        }
+
+        console.log('-> acceptAsync: everything');
+        return client.acceptAsync({ requestList: acceptAsync });
+    })
+    .then(function() {
+        console.log('-> getVersionInfo');
+        return client.getVersionInfo();
+    })
+    .then(function(info) {
+        console.log('<- Protocol version: %s', info.protocolVersion);
+        console.log('<- Server software:  %s', info.serverSoftware);
+        console.log('<- Software version: %s', info.softwareVersion);
+
+        console.log('-> getTime & getDate');
+        return Promise.props({ time: client.getTime(),
+                               date: client.getDate()});
+    })
+    .then(function(v) {
+        console.log('<- %s DOW %s DOY %s DST: %s',
+                    v.date, v.time.dayOfWeek, v.time.dayOfYear, v.time.isDST);
+    })
+    .then(function() {
+        console.log('Closing connection');
+        client.close();
+    })
+    .catch(function(err) {
+        console.log('<- Request error: %j', err);
+    });
