f71dc7effb86bbe36eaa77598d41e800675b3099
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-10-06 18:23:26 +0200

    Populate library interface

--- index.js
+++ index.js
@@ -10,31 +10,78 @@
 var net = require('net');
 var Promise = require('bluebird');
 
+var errors = require('./lib/errors');
+var protocol = require('./lib/protocol');
 var Client = require('./lib/client');
 
-/** connect(options)
- *
- * Connect to a LysKOM server.
+//
+// Main interface
+//
+
+/** Connect to a LysKOM server.
  *
  * options:
  *  - host: server host (default 'localhost')
  *  - port: server port (default 4894)
  *
  * Return:
- *  - Bluebird promise that resolves to the new Client object
+ *  - Client object, which will emit 'connect' when it is ready to be used.
  */
-module.exports = function(options) {
+var connect = exports.connect = function(options) {
     if (!options.port) {
         options.port = 4894;
     }
 
-    return new Promise(function(resolve, reject) {
-        var socket = net.connect(options);
+    return new Client(net.connect(options));
+};
 
-        socket.on('error', reject);
-        socket.once('connect', function() {
-            socket.removeListener('error', reject);
-            resolve(new Client(socket));
+
+/** Connect to a LysKOM server and wait for the connection to be ready.
+ *
+ * See connect() for the options.
+ *
+ * Return:
+ *  - Bluebird Promise, which resolves to the Client object when it
+ *    has connected and is ready to be used
+ */
+exports.connectAndWait = function(options) {
+    return new Promise(function(resolve, reject) {
+        var client = connect(options);
+
+        client.on('error', reject);
+        client.once('connect', function() {
+            client.removeListener('error', reject);
+            resolve(client);
         });
     });
 };
+
+
+//
+// Expose error classes
+//
+
+exports.ProtocolError = errors.ProtocolError;
+exports.ServerError = errors.ServerError;
+exports.ClientError = errors.ClientError;
+exports.RequestError = errors.RequestError;
+
+
+/** Map of async message names to numbers and vice versa to support
+ * usage of Client.acceptAsync/queryAsync.
+ *
+ * Names are exposed both in protocol form (foo-bar) and camel cased (fooBar)
+ */
+exports.async = {};
+
+var camelCaseName = function(name) {
+    return name.replace(/-[a-z]/g, function(s) { return s[1].toUpperCase(); });
+};
+
+for (var num in protocol.async) {
+    if (protocol.async.hasOwnProperty(num)) {
+        var a = protocol.async[num];
+        exports.async[num] = a.name;
+        exports.async[a.name] = exports.async[camelCaseName(a.name)] = parseInt(num, 10);
+    }
+}
